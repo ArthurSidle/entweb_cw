d6cca292ace175d400b18e6b72fc83e95abec513
Arthur <sidle.arthur@gmail.com>
2023-04-13 11:05:21 +0100

    basic react page

--- presentation/src/index.js
+++ presentation/src/index.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const jsx = (
+<>
+  <h1>Enter details:</h1>
+  <ProjectInfoForm/>
+</>
 );
+
+function ProjectInfoForm() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs(values => ({...values, [name]: value}));
+  }
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    alert(`Man hours: ${inputs.manHours}\nHourly rate: ${inputs.hourly}`); 
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Man hours:
+        <input
+          type="text"
+          value={inputs.manHours}
+          onChange={handleChange}></input>
+      </label>
+      <label>
+        Hourly rate:
+        <input
+          type="text"
+          value={inputs.hourly}
+          onChange={handleChange}></input>
+      </label>
+      <input type="submit"></input>
+    </form>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(jsx);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
